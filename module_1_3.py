dc88294c7d01e2187a9e3d2878fdbe2638428d36
Grigory56 <95852762+Grigory56@users.noreply.github.com>
2024-09-11 12:25:16 +0700

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,11 @@
+from operator import truediv
+
+name="Григорий"
+print('Name:',name)
+age=67
+print('Age:',age)
+age=age+1
+print('New age:',age)
+is_student=True
+print('Is Student:',is_student)
+
